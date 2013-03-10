50652b8542f5bc85f91c3994a4db94ccf568feda
Antti-Jussi Kovalainen <ajk@ajk.fi>
2013-03-10 17:59:17 +0200

    Refactor require-config to be more verbose

--- js/require-config.js
+++ js/require-config.js
@@ -1,25 +1,31 @@
 var require = {
     shim: {
-        'helpers/helpers': ['jquery'],
-        'slickgrid': [
-            'jquery',
-            'lib/SlickGrid/lib/jquery.event.drag-2.0.min',
-            'lib/SlickGrid/slick.core',
-            'lib/SlickGrid/slick.dataview',
-            'lib/SlickGrid/plugins/slick.rowselectionmodel'
-        ],
-        'soundmanager2': [],
-        'soundmanager2-audiomodule': ['soundmanager2']
+        'helpers/helpers': {
+            deps: ['jquery']
+        },
+        'slickgrid': {
+            deps: [
+                'jquery',
+                'lib/SlickGrid/lib/jquery.event.drag-2.0.min',
+                'lib/SlickGrid/slick.core',
+                'lib/SlickGrid/slick.dataview',
+                'lib/SlickGrid/plugins/slick.rowselectionmodel'
+            ]
+        },
+        'soundmanager2': {},
+        'soundmanager2-audiomodule': {
+            deps: 'soundmanager2'
+        }
     },
 
     paths: {
         'jquery': 'lib/jquery.min',
         'jquery-ui': 'lib/jquery-ui',
         'mediator': 'lib/mediator.min',
+        'pathjs': 'lib/path.min',
         'slickgrid': 'lib/SlickGrid/slick.grid',
         'soundmanager2': 'lib/SoundManager2/script/soundmanager2', //-nodebug-jsmin',
         'store': 'lib/store.min',
-        'pathjs': 'lib/path.min',
         'transparency': 'lib/transparency.min'
     },
 
