d2dbf941298964deef9511c7fabc7bbe2f057312
Antti-Jussi Kovalainen <ajk@ajk.fi>
2013-04-28 09:57:05 +0300

    Code clean-up

--- js/app.js
+++ js/app.js
@@ -30,11 +30,11 @@ require([
     });
 
     req.success(function (profile) {
-        usermenu  = new UserMenu({ el: $('#user-menu'), name: profile.username });
-        usermenu_listener = new UserMenuListener();
-        player    = new Player({ el: $('.app-now-playing') });
-        playlistManager = new PlaylistManager({ el: $('.main-wrap'), api: api });
-        sidebar = new Sidebar({ el: $('.app-nav') });
+        usermenu            = new UserMenu({ el: $('#user-menu'), name: profile.username });
+        usermenu_listener   = new UserMenuListener();
+        player              = new Player({ el: $('.app-now-playing') });
+        playlistManager     = new PlaylistManager({ el: $('.main-wrap'), api: api });
+        sidebar             = new Sidebar({ el: $('.app-nav') });
 
         resizer   = new Resizer();
         lastfm    = new LastFM({ api: api });
